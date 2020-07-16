6593d57402130356eee00f68a560e60d57b04ef6
AndriiNyzhnyk1994 <prostoandrew1994@gmail.com>
2020-07-16 10:51:49 +0300

    Updates

---
+++ precache-manifest.c8b45ed83fe52162517af9693804b31d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cefd17f72e3203838f998cbcdcd43e",
+    "url": "/hooks-counter/index.html"
+  },
+  {
+    "revision": "f7a45b79a1ce1a4518de",
+    "url": "/hooks-counter/static/css/main.8e0e84c3.chunk.css"
+  },
+  {
+    "revision": "0a8c2cde66d9ad260faf",
+    "url": "/hooks-counter/static/js/2.b64a4f41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hooks-counter/static/js/2.b64a4f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a45b79a1ce1a4518de",
+    "url": "/hooks-counter/static/js/main.f72bd14e.chunk.js"
+  },
+  {
+    "revision": "1c7d27c682158437aa37",
+    "url": "/hooks-counter/static/js/runtime-main.88e27496.js"
+  }
+]);
